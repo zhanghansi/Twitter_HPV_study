28ef2e090170929341a58ab07cb081a34545c834
Hansi Zhang <hansi.zhang@ufl.edu>
2017-11-20 10:36:30 -0500

    sample file for H score

--- H_score.py
+++ H_score.py
@@ -82,6 +82,7 @@ def group_tweets_by_cluster_gold_standard(source, k):
 
 def generate_tweets_by_cluster_not_gold_standard(source, k):
     tweets_pz_d = []
+    print(source)
     with open(source + str(k) + 'tp/model/k' + str(k) + '.pz_d') as topics_distribution:
         for line in topics_distribution:
             pz_ds = []
@@ -216,7 +217,7 @@ if __name__ == '__main__':
     #step 1 generate clusters for gold standard
     # group_tweets_by_cluster_gold_standard('./intermediate_data/hpv_tweets/hpv_tweets_not_by_uid.txt', k)
 
-    # generate_tweets_by_cluster_not_gold_standard('./BTM/output/',k)
+    # generate_tweets_by_cluster_not_gold_standard('./Biterm/output/',40)
 
 
     #step 2 generate word_id for BTM
